c58dfcc651076a8b39af04d2dfca9d40d306fda1
Tatiana Kustova <kunuca@mail.ru>
2023-03-02 19:18:05 +0400

    Homework

---
+++ 002/Program.cs
@@ -0,0 +1,34 @@
+﻿// Написать программу масштабирования фигуры.
+//Тут для тех кто далеко улетел, чтобы задавались вершины
+// фигуры списком (одной строкой)
+// например: "(0,0) (2,0) (2,2) (0,2)"
+//коэффициент масштабирования k задавался отдельно - 2 или 4 или 0.5
+void PrintIn(string message)
+{
+    Console.WriteLine(message);
+}
+
+
+PrintIn("Введите координаты фигуры");
+string input=Console.ReadLine();
+PrintIn("Введите коэфициент масштабирования");
+int k=int.Parse(Console.ReadLine());
+int count=input.Length;
+char output=input[0];
+for (int i = 0; i < count; i=i+5)
+{
+       
+      //1ый из выживыших вариантов
+      //output=output+input[i]+input[i+1]*k+input[i+2]+input[i+3]*k+input[i+4];
+     
+     //2о1 вариант, на коэффициенте 0,5 сломался
+      Console.Write($"({input[i+1]*k},{input[i+3]*k})");
+
+       //3ий из выживших вариантов, не может Char() в double перевести, хотя в этой версии Char переменных вообще нет >_<
+       //double r1=Convert.ToDouble(input[i+1]);
+       //double r2=Convert.ToDouble(input[i+3]);
+       //Console.Write($"({r1*k},{r2*k})");
+       
+
+        
+}
---
+++ 003/Program.cs
@@ -0,0 +1,36 @@
+﻿// Написать программу копирования массива
+void FillArray(int[] numbers)
+{
+    int length=numbers.Length;
+    for (int i = 0; i < length; i++)
+    {
+        numbers[i]=new Random().Next(1,100);
+    }
+}
+
+void FillCopy(int[] numbers,int[] copy)
+{
+    int len=numbers.Length;
+    for (int i = 0; i < len; i++)
+    {
+        copy[i]=numbers[i];
+    }
+}
+
+void PrintIn(int[] mass)
+{
+    int l=mass.Length;
+    for (int i = 0; i < l; i++)
+    {
+        Console.Write($"\t{mass[i]}");
+    }
+    
+}
+
+int[] array=new int[5];
+FillArray(array);
+PrintIn(array);
+Console.WriteLine();
+int[] dubl=new int[5];
+FillCopy(array, dubl);
+PrintIn(dubl);
